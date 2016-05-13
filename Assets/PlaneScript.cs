c1282506b831594d6c60d505f3716fd56bb480e1
Eric Howard <howard.eric.m@gmail.com>
2016-05-13 08:18:15 -0700

    added proto project

---
+++ Assets/PlaneScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneScript : MonoBehaviour {
+
+	float
+	Rotation = 500f,
+	Thrust = 25f
+	;
+
+	Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 move = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+		rigidbody.AddTorque(0,0,-move.x*Rotation*Time.deltaTime);
+			
+		Vector3 forward = transform.localRotation * new Vector3(1,0,1);
+		Debug.Log(forward);
+		if(move.y > 0) {
+			rigidbody.AddForce( forward * Thrust );
+		}
+	}
+}
